90ca08108473d933160029c702c7b45d98762e3f
Arvid van de Crommert <arvidvdc@gmail.com>
2019-10-28 14:53:16 +0100

    Laatste deel

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -3,15 +3,18 @@ var express = require("express");
 var app = express();
 var bronvermelding = '<p style="text-align: center; margin: 25px; padding: 0;"> <span style="border: solid 1px; margin: 0; padding: 0 5px;">Based on course "The Web Developer Bootcamp | Udemy" from Colt Steele</span></p>'
 
+app.use(express.static("public")); // Mappen beschikbaar maken voor express
+app.set("view engine", "ejs"); // Applicatie vertellen dat ejs bestanden gebruikt worden.
+
 // Routing
 app.get("/",(req,res) => {
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing",(req,res) => {
     var thing = req.params.thing;
 
-    res.render("love.ejs", {thingVar: thing} );
+    res.render("love", {thingVar: thing} );
 });
 
 app.get("/posts",(req,res) => {
@@ -20,7 +23,7 @@ app.get("/posts",(req,res) => {
         {title: "Biertje", author: "Arvid"},
         {title: "Dit geloof je niet", author: "Admin"}
     ];
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
 
 // Listener
